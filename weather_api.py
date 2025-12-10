00a512be2ab37c7e97169223aad0de039c14bcd2
Ancy Sneha P <ancysneha005@gmail.com>
2025-12-10 08:08:41 +0000

    upadte weather_api

--- weather_api.py
+++ weather_api.py
@@ -1,15 +1,17 @@
 import requests
-
-API_KEY = "93e34d69fdef9bf68d55427ac840479d"
+import streamlit as st
 
 def get_weather(city):
+    # Get the API key from Streamlit Secrets
+    API_KEY = st.secrets["API_KEY"]
+
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
     try:
         response = requests.get(url, timeout=5)
         data = response.json()
 
-        print("Weather API raw response:", data)  # DEBUG (remove later)
+        print("Weather API raw response:", data)  # DEBUG
 
         if data.get("cod") != 200:
             return None
